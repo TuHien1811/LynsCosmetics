38bdc9c7eedf9fe02103d32722fa925c2d8646ac
Neko Shiro <tuhien1811@gmail.com>
2021-03-18 22:36:30 +0700

    Tim Kiem

---
+++ SourceCode/TTTN_OnlineShop/Controllers/TimKiemController.cs
@@ -0,0 +1,20 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TTTN_OnlineShop.Models.BUS;
+
+namespace TTTN_OnlineShop.Controllers
+{
+    public class TimKiemController : Controller
+    {
+        // GET: TimKiem
+        public ActionResult Index(string timKiem, int page = 1, int pagesize = 12)
+        {
+            var db = TimKiemBUS.TimKiem(timKiem).ToPagedList(page, pagesize);
+            return View(db);
+        }
+    }
+}
---
+++ SourceCode/TTTN_OnlineShop/Models/BUS/TimKiemBUS.cs
@@ -0,0 +1,17 @@
+﻿using LynsCosmetics_Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TTTN_OnlineShop.Models.BUS
+{
+    public class TimKiemBUS
+    {
+        public static IEnumerable<Table_SanPham> TimKiem(string timKiem)
+        {
+            var db = new LynsCosmetics_ConnectionDB();
+            return db.Query<Table_SanPham>("select * from Table_SanPham where TenSanPham like N'%" + timKiem + "%'");
+        }
+    }
+}
